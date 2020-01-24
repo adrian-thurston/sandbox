018a1083dfbf988ad1428721adb6c5859216ee4e
Adrian Thurston <thurston@colm.net>
2020-01-24 16:09:50 +0200

    added note about testing shared libs

--- cgo/rust/flux.c
+++ cgo/rust/flux.c
@@ -6,6 +6,9 @@ void flux()
 	 * $ ./cgo
 	 *
 	 * Is there any way to do this without the 'uniq' hack in the Makefile? /
+	 *
+	 * Already tried: make it into a shared library and link with that. Go will
+	 * still link it statically and use the same rebuilding rules.
 	 */
-	printf( "flux() inside archive 12\n");
+	printf( "flux() inside archive 15\n");
 }
